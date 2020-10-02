e5dfc15beb9fe85eaca14d6ac4a9ece0b581f275
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 22:11:57 +0700

    modify and update local service

--- lib/service/service_local.dart
+++ lib/service/service_local.dart
@@ -1,23 +1,33 @@
 part of 'service.dart';
 
 class LocalService {
-  Future<void> saveWallets(List<Wallet> wallets) async {
+
+  final List<Wallet> _wallets = [];
+  List<Wallet> get wallets => _wallets;
+  
+  Future<void> saveWallets() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String encodeWallets = json.encode(wallets);
-    prefs.setString("encodeWallets", encodeWallets);
+    await prefs.setString("encodeWallets", encodeWallets);
   }
 
   Future<List<Wallet>> loadWallets() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String encodeWallets = prefs.getString("encodeWallets") ?? "{}";
-    
-    final Map<String, String> decodeWallets = json.decode(encodeWallets) as Map<String, String>;
-    final List<Wallet> wallets = [];
+    try {
 
-    for (final String walletName in decodeWallets.keys) {
-      wallets.add(Wallet.fromJson(decodeWallets[walletName] as Map<String, dynamic>));
+      await SharedPreferences.getInstance()
+        .then((prefs) {
+          final String encodeWallets = prefs.getString("encodeWallets") ?? "{}";
+          final Map<String, String> decodeWallets = json.decode(encodeWallets) as Map<String, String>;
+
+          for (final String walletName in decodeWallets.keys) {
+            _wallets.add(Wallet.fromJson(decodeWallets[walletName] as Map<String, dynamic>));
+          }
+        }).timeout(const Duration(seconds: 10));
+      
+      return _wallets;
+
+    } catch (e) {
+      return _wallets;
     }
-    return wallets;
   }
-
 }
