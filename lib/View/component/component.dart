b51bae0d76b0df621644aeb9cc29aee24280af04
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 19:34:04 +0700

    fix and update snackbar to object

--- lib/View/component/component.dart
+++ lib/View/component/component.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:mywallet/share.dart';
-import '../X.dart';
 
 part 'xtopbar.dart';
 part 'xcard.dart';
+part 'xtextfield.dart';
 
-SnackBar snackBar({
-  String contentText, String labelText, VoidCallback onPressed
-}) => SnackBar(
-  content: Text(contentText),
-  action: SnackBarAction(
-    label: labelText,
-    onPressed: onPressed,
-  ),
-);
+class XSnackBar extends SnackBar {
+  XSnackBar({
+    Key key,
+    String contentText,
+    String buttonText,
+    double elevation,
+    EdgeInsets margin,
+    EdgeInsets padding,
+    void Function() onPressed
+  }) : super(
+    key: key,
+    content: Text(contentText),
+    action: SnackBarAction(label: buttonText, onPressed: onPressed),
+    margin: margin,
+    padding: padding,
+    elevation: elevation,
+  );
+}
 
 class WalletCard<T> extends StatelessWidget {
   
@@ -29,16 +38,14 @@ class WalletCard<T> extends StatelessWidget {
   Widget build(BuildContext context) {
     return XCard(
       children: <Widget>[
-        XListTile(
+        ListTile(
           title: StreamBuilder<T>(
             stream: blocStream,
-            builder: (context, snapshot) => XText(_walletName, 
-              size: 18, isBold: true
-            )
+            builder: (context, snapshot) => Text(_walletName)
           ),
           trailing: StreamBuilder<T>(
             stream: blocStream,
-            builder: (context, snapshot) => XText(snapshot.data as String)
+            builder: (context, snapshot) => Text(snapshot.data as String)
           ),
         ),
         StreamBuilder<T>(
@@ -48,23 +55,18 @@ class WalletCard<T> extends StatelessWidget {
             itemBuilder: (context, index) {
               const String name = "";
               const String value = "";
-              return const XListTile(
-                title: XText(name),
-                trailing: XText(value),
-                isBorder: true,
+              return const ListTile(
+                title: Text(name),
+                trailing: Text(value),
               );
             },
           )
         ),
-        XButton(
-          margin: 6,
+        FlatButton(
           color: Theme.of(context).accentColor,
           height: 40,
-          isBorder: true,
-          onTap: () => Navigator.pushNamed(context, '/details', arguments: _walletName),
-          child: XText("See Details",
-            color: (Theme.of(context).accentColorBrightness == Brightness.dark) ? Colors.white : Colors.black,
-          ),
+          onPressed: () => Navigator.pushNamed(context, '/details', arguments: _walletName),
+          child: const Text("See Details",),
         ),
       ],
     );
@@ -74,16 +76,22 @@ class WalletCard<T> extends StatelessWidget {
 class ColumnBuilder extends StatelessWidget {
   final IndexedWidgetBuilder itemBuilder;
   final int itemCount;
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
 
   const ColumnBuilder({
     Key key,
     @required this.itemBuilder,
     @required this.itemCount,
+    this.mainAxisAlignment,
+    this.crossAxisAlignment
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.start,
+      crossAxisAlignment: crossAxisAlignment ?? CrossAxisAlignment.center,
       children: List.generate(
         itemCount, (index) => itemBuilder(context, index)).toList(),
     );
