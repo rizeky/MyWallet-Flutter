cf54ba8bca47e69d8b5178bc22619a7f95e3c910
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 22:07:15 +0700

    add setup locator function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mywallet/locator.dart';
 import 'router.dart';
 
 Future<void> main() async {
+  setupLocator();
+  await awaitSetupLocator();
   runApp(MyApp());
 }
 
