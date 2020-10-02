d248f6959b0f94e3142d7d3afa0121e9468055e1
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 22:14:50 +0700

    modify and update home page

--- lib/View/page_home.dart
+++ lib/View/page_home.dart
@@ -4,33 +4,34 @@ class HomePage extends Page<HomeBloc> {
   HomePage({Key key}) : super(key: key);
   
   @override
-  void dispose() {
-    // TODO: implement dispose
-  }
+  void dispose() {}
 
   @override
-  void init() {
-    // TODO: implement init
-  }
+  void init() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: XTopBar(
-        textTitle: "",
+        title: ValueListenableBuilder<String>(
+          valueListenable: _bloc.notifierTotalWalletCurr, 
+          builder: (context, value, child) => Text(value)),
         leading: const Icon(Icons.account_balance_wallet),
-        trailing: IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () => Navigator.pushNamed(context, '/input'),
-        ),  
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () => Navigator.pushNamed(context, '/inputwallet'),
+          )
+        ],  
       ),
       body: FutureBuilder<List<Wallet>>(
-        initialData: const [],
-        future: bloc.getWallets(),
-        builder: (context, snapshot) => (snapshot.hasData) ? ListView.builder(
+        future: _bloc.getWallets(),
+        builder: (context, snapshot) => (snapshot.hasData) ? 
+          (snapshot.data.isNotEmpty) ? ListView.builder(
             itemCount: snapshot.data.length,
             itemBuilder: (context, index) => WalletCard(snapshot.data[index])
-          ) : const Center(child: CircularProgressIndicator(),)
+          ) : const Center(child: Text('No wallets found')) 
+          : const Center(child: CircularProgressIndicator(),)
       ),
     );
   }
