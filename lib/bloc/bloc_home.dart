06f6fc26ecbdb9fa3cbd97bd33b9d392dedb06cd
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 22:10:31 +0700

    add value notifier for total wallet curr

--- lib/bloc/bloc_home.dart
+++ lib/bloc/bloc_home.dart
@@ -4,6 +4,7 @@ part of 'bloc.dart';
 class HomeBloc implements Bloc {
 
   final LocalService _localService = locator.get<LocalService>();
+  final ValueNotifier<String> notifierTotalWalletCurr = ValueNotifier("Rp0");
 
   @override
   void dispose() {}
@@ -14,11 +15,20 @@ class HomeBloc implements Bloc {
   final NumberFormat _numRpCurr = NumberFormat.simpleCurrency(name: "Rp", decimalDigits: 0);
   
   final List<Wallet> _wallets = [];
+  int _totalWallet = 0;
   // List<Wallet> get wallets => _wallets;
 
   Future<List<Wallet>> getWallets() async {
     await _localService.loadWallets().then((value) => _wallets.addAll(value));
+    for (final Wallet wallet in _wallets) {
+      _totalWallet += wallet.totalValue;
+    }
+    notifierTotalWalletCurr.value = totalWalletCurr();
     return _wallets;
+  }
+
+  String totalWalletCurr() {
+    return _numRpCurr.format(_totalWallet);
   }
 }
 
