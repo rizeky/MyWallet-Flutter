518fbf7316c8a960e94061d83daecadfb965d152
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 20:07:08 +0700

    modify and update home page

--- lib/View/page_home.dart
+++ lib/View/page_home.dart
@@ -1,7 +1,17 @@
 part of 'page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends Page<HomeBloc> {
   HomePage({Key key}) : super(key: key);
+  
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +24,15 @@ class HomePage extends StatelessWidget {
           onPressed: () => Navigator.pushNamed(context, '/input'),
         ),  
       ),
-      body: Center(),
+      body: FutureBuilder<List<Wallet>>(
+        initialData: const [],
+        future: bloc.getWallets(),
+        builder: (context, snapshot) => (snapshot.hasData) ? ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) => WalletCard(snapshot.data[index])
+          ) : const Center(child: CircularProgressIndicator(),)
+      ),
     );
   }
+
 }
