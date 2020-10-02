a4cca1bb11875765520289f5c7ab611a8d9fb678
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 22:54:07 +0700

    modify and update home page

--- lib/View/page_home.dart
+++ lib/View/page_home.dart
@@ -16,13 +16,7 @@ class HomePage extends Page<HomeBloc> {
         title: ValueListenableBuilder<String>(
           valueListenable: _bloc.notifierTotalWalletCurr, 
           builder: (context, value, child) => Text(value)),
-        leading: const Icon(Icons.account_balance_wallet),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () => Navigator.pushNamed(context, '/inputwallet'),
-          )
-        ],  
+        leading: const Icon(Icons.account_balance_wallet), 
       ),
       body: FutureBuilder<List<Wallet>>(
         future: _bloc.getWallets(),
@@ -32,6 +26,55 @@ class HomePage extends Page<HomeBloc> {
             itemBuilder: (context, index) => WalletCard(snapshot.data[index])
           ) : const Center(child: Text('No wallets found')) 
           : const Center(child: CircularProgressIndicator(),)
+      ),
+      floatingActionButton: Builder(
+        builder: (scaffContext) => FloatingActionButton(
+          elevation: 0,
+          highlightElevation: 0,
+          onPressed: () => showModalBottomSheet(
+            context: scaffContext,
+            builder: (context) {
+
+              final TextEditingController walletNameCon = TextEditingController();
+
+              return Material(
+                color: Colors.white,
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Input Wallet Name', style: blackSubtitle),
+                      const SizedBox(height: 12,),
+                      XTextField(
+                        labelText: 'Wallet Name',
+                        labelStyle: blackSubtitle,
+                        controller: walletNameCon,
+                      ),
+                      const SizedBox(height: 12,),
+                      ButtonBar(
+                        children: [
+                          FlatButton(
+                            minWidth: 50,
+                            onPressed: () {},
+                            child: Text('CANCEL', style: blackContentRegular,),
+                          ),
+                          FlatButton(
+                            minWidth: 50,
+                            color: mainColor,
+                            onPressed: () {},
+                            child: const Text('CREATE'),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                )
+              );
+            }
+          ),
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
