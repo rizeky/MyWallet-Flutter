90f8c876a91cfac020d3cce2b149d75fcbfe16f7
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-02 20:05:55 +0700

    update bloc

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,8 +1,8 @@
 import 'package:rxdart/rxdart.dart';
 import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:mywallet/locator.dart';
+import 'package:mywallet/service/service.dart';
 import 'package:mywallet/model/model.dart';
 
 part 'bloc_home.dart';
