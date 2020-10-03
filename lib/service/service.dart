5bc1c2c624235b1d9072098035d53f0676af6836
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-03 19:03:32 +0700

    update service

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:mywallet/model/model.dart';
 
