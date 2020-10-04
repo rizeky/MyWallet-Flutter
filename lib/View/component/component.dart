0653acc84675e36730275c3894e92ad3ccb24459
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-04 10:49:09 +0700

    add xdialog object and fix wallet card

--- lib/View/component/component.dart
+++ lib/View/component/component.dart
@@ -27,12 +27,75 @@ class XSnackBar extends SnackBar {
   );
 }
 
+class XDialog extends StatelessWidget {
+
+  final String name;
+  final void Function(String commit, int value) onSubmit;
+  final void Function() onCancel;
+
+  const XDialog({
+    Key key,
+    this.name,
+    this.onSubmit,
+    this.onCancel
+  }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final TextEditingController _textCommitController = TextEditingController();
+    final TextEditingController _textNumController = TextEditingController();
+
+    return AlertDialog(
+      elevation: 0,
+      title: Text(name),
+      content: SizedBox(
+        height: 120,
+        child: Column(
+          children: [
+            TextField(
+              controller: _textCommitController,
+              decoration: const InputDecoration(hintText: "Input commit"),
+            ),
+            const SizedBox(height: 12,),
+            TextField(
+              keyboardType: TextInputType.number,
+              controller: _textNumController,
+              decoration: const InputDecoration(hintText: "Input value"),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            if (onCancel != null) onCancel();
+            Navigator.of(context).pop();
+          },
+          height: 40,
+          child: const Text('CANCEL'),
+        ),
+        FlatButton(
+          color: mainColor,
+          onPressed: () {
+            if (onSubmit != null) onSubmit(_textCommitController.text, int.parse(_textNumController.text));
+            Navigator.of(context).pop();
+          },
+          child: const Text("SUBMIT"),
+        )
+      ],
+    );
+  }
+}
+
 class WalletCard extends StatelessWidget {
   
   const WalletCard(this._wallet, {
     Key key,
+    this.onSubmit
   }) : super(key: key);
 
+  final void Function(String, int) onSubmit;
   final Wallet _wallet;
 
   @override
@@ -46,7 +109,14 @@ class WalletCard extends StatelessWidget {
             subtitle: Text(convertCurrRP(_wallet.totalValue), style: whiteNum),
             trailing: IconButton(
               icon: Icon(Icons.add, color: whiteColor), 
-              onPressed: () {},
+              onPressed: () => showDialog(
+                context: context, 
+                builder: (context) => XDialog(
+                  name: _wallet.name, 
+                  onSubmit: (commit, value) {
+                    
+                  },) 
+                ),
             ),
           ),
           Divider(color: whiteColor, indent: 12, endIndent: 12, height: 0,),
@@ -57,8 +127,8 @@ class WalletCard extends StatelessWidget {
                 final String name = _wallet.details.keys.toList()[index] as String;
                 final int value = _wallet.details.values.toList()[index] as int;
                 return ListTile(
-                  leading: (value < 0) ? const Icon(Icons.remove, size: 18,) 
-                    : const Icon(Icons.add, size: 18),
+                  leading: (value < 0) ? const Icon(Icons.remove, color: Colors.red,) 
+                    : const Icon(Icons.add, color: Colors.green),
                   title: Text(name),
                   trailing: Text(convertCurrRP(value)),
                 );
