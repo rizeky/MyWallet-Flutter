b350e9b1df67e08defc95f8417fd5a9ab05c5f1c
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-04 09:31:11 +0700

    add text style and method for share

--- lib/share.dart
+++ lib/share.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/painting.dart';
+import 'package:intl/intl.dart';
 
 Color mainColor = const Color(0xFF6200EE);
 Color accentColor = const Color(0xFF03DAC5);
@@ -12,13 +13,27 @@ TextStyle whiteTitle = TextStyle(
     color: whiteColor, fontSize: 24, fontWeight: FontWeight.bold);
 
 TextStyle blackSubtitle = TextStyle(
-    color: blackColor, fontSize: 18, fontWeight: FontWeight.normal);
+    color: blackColor, fontSize: 18, fontWeight: FontWeight.bold);
 
 TextStyle blueSubtitle = TextStyle(
     color: mainColor, fontSize: 18, fontWeight: FontWeight.bold);
 
 TextStyle whiteSubtitle = TextStyle(
-    color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold);
+    color: whiteColor, fontSize: 18, fontWeight: FontWeight.normal);
 
 TextStyle blackContentRegular = TextStyle(
     color: blackColor, fontSize: 16, fontWeight: FontWeight.normal);
+
+TextStyle whiteContentRegular = TextStyle(
+    color: whiteColor, fontSize: 16, fontWeight: FontWeight.normal);
+
+TextStyle whiteNum = TextStyle(
+    color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold);
+
+TextStyle blackNum = TextStyle(
+    color: blackColor, fontSize: 18, fontWeight: FontWeight.bold);
+
+String convertCurrRP(int value) {
+  final NumberFormat formatCurrency = NumberFormat.simpleCurrency(name: 'IDR', decimalDigits: 0);
+  return formatCurrency.format(value);
+}
