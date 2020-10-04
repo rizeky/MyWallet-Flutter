3fb73e9887b9d3370f6ffa54d7c2076d69db1558
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-04 09:33:18 +0700

    modify and fix home page

--- lib/View/page_home.dart
+++ lib/View/page_home.dart
@@ -18,68 +18,76 @@ class HomePage extends Page<HomeBloc> {
           builder: (context, value, child) => Text(value)),
         leading: const Icon(Icons.account_balance_wallet), 
       ),
-      body: FutureBuilder<List<Wallet>>(
-        future: _bloc.getWallets(),
-        builder: (context, snapshot) => (snapshot.hasData) ? 
-          (snapshot.data.isNotEmpty) ? ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (context, index) => WalletCard(snapshot.data[index])
-          ) : const Center(child: Text('No wallets found'))
-          : const Center(child: CircularProgressIndicator(),)
+      body:StreamBuilder<List<Wallet>>(
+        stream: _bloc.walletsStream,
+        builder: (context, snapshot) => (snapshot.hasData) ? Padding(
+          padding: const EdgeInsets.all(24),
+          child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(height: 24),
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) => WalletCard(snapshot.data[index])
+            ),
+        ) : const Center(child: CircularProgressIndicator(),),
       ),
       floatingActionButton: Builder(
         builder: (scaffContext) => FloatingActionButton(
           elevation: 0,
           highlightElevation: 0,
           onPressed: () => showModalBottomSheet(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(25.0))),
+            isScrollControlled: true,
             context: scaffContext,
             builder: (context) {
 
               final TextEditingController walletNameCon = TextEditingController();
               final TextEditingController walletValueCon = TextEditingController();
 
-              return Material(
-                color: Colors.white,
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Input Wallet Name', style: blackSubtitle),
-                      const SizedBox(height: 12,),
-                      XTextField(
-                        labelText: 'Wallet Name',
-                        labelStyle: blackSubtitle,
-                        controller: walletNameCon,
-                      ),
-                      const SizedBox(height: 12,),
-                      XTextField(
-                        labelText: 'Wallet Value',
-                        labelStyle: blackSubtitle,
-                        controller: walletValueCon,
-                      ),
-                      const SizedBox(height: 12,),
-                      ButtonBar(
-                        children: [
-                          FlatButton(
-                            minWidth: 50,
-                            onPressed: () => Navigator.pop(context),
-                            child: Text('CANCEL', style: blackContentRegular,),
-                          ),
-                          FlatButton(
-                            minWidth: 50,
-                            color: mainColor,
-                            onPressed: () {
-
-                              Navigator.pop(context);
-                            },
-                            child: const Text('CREATE'),
-                          ),
-                        ],
-                      )
-                    ],
+              return Padding(
+                padding: MediaQuery.of(context).viewInsets,
+                child: Material(
+                  color: blackColor,
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Input Wallet', style: whiteSubtitle),
+                        const SizedBox(height: 12,),
+                        XTextField(
+                          labelText: 'Wallet Name',
+                          labelStyle: whiteSubtitle,
+                          controller: walletNameCon,
+                        ),
+                        const SizedBox(height: 12,),
+                        XTextField(
+                          labelText: 'Wallet Value',
+                          labelStyle: whiteSubtitle,
+                          controller: walletValueCon,
+                        ),
+                        const SizedBox(height: 12,),
+                        ButtonBar(
+                          children: [
+                            FlatButton(
+                              minWidth: 50,
+                              onPressed: () => Navigator.pop(context),
+                              child: Text('CANCEL', style: whiteContentRegular,),
+                            ),
+                            FlatButton(
+                              minWidth: 50,
+                              color: mainColor,
+                              onPressed: () {
+                                _bloc.addWallet(walletNameCon.text, int.parse(walletValueCon.text));
+                                Navigator.pop(context);
+                              },
+                              child: const Text('CREATE'),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
                   )
-                )
+                ),
               );
             }
           ),
